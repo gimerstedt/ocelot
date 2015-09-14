c1ee06980a4c1383bfa275371b4aba49f305bbc1
Rickard Gimerstedt <rickard@gimerstedt.se>
2015-09-14 21:59:20 +0000

    removing tests

--- clojure/test/ocelot/core_test.clj
+++
@@ -1,8 +0,0 @@
-(ns ocelot.core-test
-  (:require [clojure.test :refer :all]
-            [ocelot.core :refer :all]))
-
-(deftest hello-world-test
-	(testing "making sure it returns hello world"
-		(is (= "Hello, you!" (ocelot.core/hello "you")))))
-
